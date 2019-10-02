4ac0e78e2504249e8d97bb0587fe1e941f4ecfd6
Neha Lal <rani259@hotmail.com>
2019-10-02 11:05:11 -0700

    created deploy framework

---
+++ seeds.sql
@@ -0,0 +1,7 @@
+USE greatbay_db;
+
+INSERT INTO auctions(name, category,price)
+VALUES ("Girl Scout Cookies","food",5.99);
+
+INSERT INTO auctions(name, category,price)
+VALUES ("lamp","furniture",12);
